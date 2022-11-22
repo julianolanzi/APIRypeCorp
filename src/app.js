2577f5920b04ffd1d205563c3261de86bc158ad0
julianolanzi <julianolanzi@hotmail.com>
2022-11-22 12:15:39 -0300

    api is running

---
+++ src/app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require("path");
+
+
+const app = express();
+
+const indexRoute = require('./app/routes/index.routes');
+
+
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(
+    "/files",
+    express.static(path.resolve(__dirname, "tmp", "uploads"))
+);
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
+app.use('/', indexRoute);
+
+module.exports = app;
