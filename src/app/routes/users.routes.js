3f3b63239c433efa6b640b352cbf6fa16c6e3a9e
julianolanzi <julianolanzi@hotmail.com>
2022-12-19 14:14:19 -0300

    public route user id

--- src/app/routes/users.routes.js
+++ src/app/routes/users.routes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const controller = require('../controllers/user-controller');
 const middlewares = require('../middlewares/auth');
 
-router.get('/', middlewares.verifyToken, controller.get);
+router.get('/', middlewares.isAdmin, controller.get);
 router.post('/', controller.post);
-router.get('/:id', middlewares.isAdmin, controller.getById);
+router.get('/:id', middlewares.verifyToken, controller.getById);
 router.put('/:id', middlewares.verifyToken, controller.put);
 router.delete('/:id', middlewares.verifyToken, controller.delete);
 
