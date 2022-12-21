b812c104f13a7080754599a08154f5a867262dc2
julianolanzi <julianolanzi@hotmail.com>
2022-12-21 12:12:52 -0300

    ajuste de roles

--- src/app/routes/users.routes.js
+++ src/app/routes/users.routes.js
@@ -5,9 +5,9 @@ const middlewares = require('../middlewares/auth');
 
 router.get('/', middlewares.isAdmin, controller.get);
 router.post('/', controller.post);
-router.get('/:id', middlewares.isAdmin, controller.getById);
-router.put('/:id', middlewares.isAdmin, controller.put);
-router.put('/updatepass/:id', middlewares.isAdmin, controller.UpdatePassword);
-router.delete('/:id', middlewares.isAdmin, controller.delete);
+router.get('/:id', middlewares.verifyToken, controller.getById);
+router.put('/:id', middlewares.verifyToken, controller.put);
+router.put('/updatepass/:id', middlewares.verifyToken, controller.UpdatePassword);
+router.delete('/:id', middlewares.verifyToken, controller.delete);
 
 module.exports = router;
